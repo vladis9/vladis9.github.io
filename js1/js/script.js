d67618d333ab546f628f1931ae5c506115d60a87
vladis9 <62884992+vladis9@users.noreply.github.com>
2020-08-02 17:28:04 +0300

    Add files via upload

---
+++ js1/js/script.js
@@ -0,0 +1,43 @@
+// console.log(1); --- view in console
+"use strict";
+
+// let number = 4; /*number*/
+
+// const persone = 'Alex'; /*string*/
+
+// const bool = true / false; /*boolean*/
+
+// console.log(something); /*null*/
+
+// let und;
+// console.log(und); /*undefined*/
+
+// const obj = {         /*object*/
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.png', {}, []];  /*massive*/
+// console.log(arr[2]);
+
+// alert('hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('18?', '');
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt('what is your name?', '');
+// answers[1] = prompt('what is your surname?', '');
+// answers[2] = prompt('age?', '');
+
+// document.write(answers);
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+//
+const user = 'Vlad';
+alert(`hello, ${user}`);
